a15df6271b2369a33be7c510755ca01d341509fb
Ray Auxey <rayauxey@gmail.com>
2019-01-14 14:54:00 +0300

    q

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -14,7 +14,7 @@ export default {
     };
   },
   mounted() {
-    fetch("http://localhost:3000/api/users")
+    fetch("https://egoparkeastafrica/api/users")
       .then(res => res.json())
       .then(res => {
         this.users = res.data.users;
