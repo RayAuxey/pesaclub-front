a7d06879241666619d3d16d83f91b6cbd65c2a4d
Ray Auxey <rayauxey@gmail.com>
2019-01-14 14:24:17 +0300

    First Commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["msg"]
+};
+</script>
+
+<style>
+</style>
