48e210d8236ef60065e000969f8fb1501db0041c
Ray Auxey <rayauxey@gmail.com>
2019-01-15 16:13:13 +0300

    Trying Pesaform View

--- src/components/Pesaform.vue
+++ src/components/Pesaform.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Pesaform</h5>
-        <h6 class="card-subtitle">ID: {{pesaform._id}}</h6>
+        <h6 class="card-subtitle">ID: {{pesaform.ID}}</h6>
         <div class="level-list">
           <ul class="list-group">
             <li v-for="index in 8" :key="index" class="list-group-item">
@@ -26,12 +26,12 @@ export default {
     };
   },
   mounted() {
-    fetch(`https://egoparkeastafrica.com/api/pesaform/id/${this._id}`)
+    fetch(`https://egoparkeastafrica.com/api/pesaform/id/${this.ID}`)
       .then(res => res.json())
       .then(res => (this.pesaform = res))
       .catch(err => console.log(err));
   },
-  props: ["_id"]
+  props: ["ID"]
 };
 </script>
 
